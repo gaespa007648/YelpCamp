7de47326a2d20c2e7c741c564cdfb1f4479d6996
HONG-MING LIU <gaespa007648@gmail.com>
2022-01-14 22:42:25 +0800

    Adding the reviews model

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const Review = require('./modules/review');
 const Campground = require('./modules/campground');
 const path = require('path');
-const Joi = require('joi');
 const engine = require('ejs-mate');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
-const {campgroundSchema} = require('./checkSchema');
+const {campgroundSchema, reviewSchema} = require('./checkSchema');
 
-mongoose.connect('mongodb://localhost:27017/yelpcamp') // The default port of mongoDB is 27017
+mongoose.connect('mongodb://localhost:27017/yelpcamp')
     .then(() => {
         console.log('Success in connection!');
     })
@@ -25,18 +25,29 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 
-// Set the middleware the verify the new database data.
-const verifySchema = (req, res, next)=>{
+// Set the middleware the verify campground data.
+const validateCampground = (req, res, next)=>{
     const { error } = campgroundSchema.validate(req.body);
     if (error) {
         const msg = error.details.map(el => el.message).join(',');
         throw new ExpressError(msg, 400); // StatusCode=400 means bad request
+    }else{
+        next();
     }
-    next();
+}
+
+// Set the middleware to verify review data
+const validateReview = (req, res, next)=>{
+    const {error} = reviewSchema.validate(req.body);
+    if(error){
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400); // StatusCode=400 means bad request
+    }else{
+        next();
+    }
 }
 
 app.get('/campground', catchAsync( async(req, res)=>{
-    
     const campgrounds = await Campground.find({});
     res.render('campground/index', {campgrounds});
 }))
@@ -46,7 +57,7 @@ app.get('/campground/new', catchAsync( async(req, res)=>{
 }))
 
 app.get('/campground/:id', catchAsync( async(req, res, next)=>{
-    const campground = await Campground.findById(req.params.id);
+    let campground = await Campground.findById(req.params.id).populate('reviews');
     res.render('campground/show', {campground});
 }))
 
@@ -55,14 +66,23 @@ app.get('/campground/:id/edit', catchAsync( async(req, res, next)=>{
     res.render('campground/edit', { campground });
 }))
 
-app.post('/campground', verifySchema, catchAsync( async(req, res, next)=>{
+app.post('/campground/:id/reviews', validateReview, catchAsync( async(req, res, next)=>{
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campground/${req.params.id}`);
+}))
+
+app.post('/campground', validateCampground, catchAsync( async(req, res, next)=>{
     // if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campground/${campground._id}`);
 }))
 
-app.put('/campground/:id', verifySchema, catchAsync( async(req, res, next)=>{
+app.put('/campground/:id', validateCampground, catchAsync( async(req, res, next)=>{
     const id = req.params.id;
     await Campground.findByIdAndUpdate(id, req.body.campground);
     res.redirect(`/campground/${id}`);
@@ -72,6 +92,13 @@ app.delete('/campground/:id', catchAsync( async(req, res, next)=>{
     const id = req.params.id;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campground');
+}))
+
+app.delete('/campground/:id/reviews/:reviewId', catchAsync( async(req, res, next)=>{
+    const {id, reviewId} = req.params;
+    await Campground.findOneAndUpdate(id, {$pull: {reviews: reviewId}});
+    await Review.findByIdAndRemove(reviewId);
+    res.redirect(`/campground/${id}`);
 }))
 
 // If the website url doesn't match any app, it will run this function
--- modules/campground.js
+++ modules/campground.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Review = require('./review');
 const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
@@ -6,7 +7,24 @@ const CampgroundSchema = new Schema({
     price: Number,
     description: String,
     location: String,
-    image: String
+    image: String,
+    reviews: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Review'
+    }]
 });
+
+// Schema.post will be trigger when you call 'findOneAndDelete'
+// The deleted instance will be passed in doc.
+// This function is belonged to query middleware. We will get warning...
+CampgroundSchema.post('findOneAndDelete', async function(doc){
+    if(doc){
+        await Review.remove({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
+})
 
 module.exports = mongoose.model('Campground', CampgroundSchema);
